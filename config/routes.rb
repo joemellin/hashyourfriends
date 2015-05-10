d64b572a800eda8292ddf70317e0495bbbc7f7ef
Dan Tripp <rubyfolksinger@gmail.com>
2015-05-09 20:42:57 -0700

    Hello OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'static_pages/about'
+  get 'static_pages/inside'
 
   root to: "static_pages#home"
   
